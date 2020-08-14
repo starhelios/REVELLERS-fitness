513c6bab2d8eb2909f08ac3facf4655f610bacab
Patrick Sam <patrickmcauley814@gmail.com>
2020-08-14 10:10:14 -0400

    Stripe connection issue bypass

--- src/functions/createCustomer.js
+++ src/functions/createCustomer.js
@@ -1,30 +1,59 @@
 const stripe = require('stripe')(process.env.GATSBY_STRIPE_SECRET_KEY);
-export function handler(event, context, callback) {
-    const data = JSON.parse(event.body);
-    const { email, name } = data;
 
-    stripe.customers
+function stripeCreate(email, name) {
+    return new Promise((resolve, reject) => {
+        stripe.customers
         .create({
             email,
             name,
         })
         .then(customer => {
-            console.log('createCustomer(), Returned Details: ', customer);
-            return callback(null, {
-                statusCode: 200,
-                body: JSON.stringify({
-                    message: 'SUCCESS: Customer Added To Stripe',
-                    customer,
-                }),
-            });
+            resolve(customer);
         })
         .catch(error => {
-            console.error('createCustomer(), ERROR: ', error);
-            return callback(null, {
-                statusCode: 401,
-                body: JSON.stringify({
-                    error: error.message,
-                }),
-            });
+            reject(error);
         });
+    })
+}
+
+function sleep() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => resolve(true), 1000);
+    })
+}
+
+export async function handler(event, context, callback) {
+    const data = JSON.parse(event.body);
+    const { email, name } = data;
+    let customer = null;
+    let error = null;
+
+    for (let i = 0; i < 5; i++) {
+        try {
+            customer = await stripeCreate(email, name);
+            break;
+        } catch (err) {
+            error = err;
+            await sleep();
+        }
+    }
+
+    if (customer) {
+        console.log('createCustomer(), Returned Details: ', customer);
+        return callback(null, {
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'SUCCESS: Customer Added To Stripe',
+                customer,
+            }),
+        });
+    } else {
+        console.error('createCustomer(), ERROR: ', error);
+        return callback(null, {
+            statusCode: 401,
+            body: JSON.stringify({
+                error: error.message,
+            }),
+        });
+    }     
 }
